4632224d6a969bde6df56347393af850576cc799
Eugene <yauhen.vasileuski1@ibm.com>
2021-03-30 02:32:25 +0300

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,52 +1,13 @@
 import React, { useState } from 'react';
 import classes from './App.module.scss';
-import { LoadFile } from './components/LoadFIle';
-import { Questions } from './components/Questions';
-import { Result } from './components/Result';
 import { Home } from './components/Home';
-import { Management } from './components/Management';
-import { Admin } from './components/Admin/Admin';
-import { UserSelectionForTesting } from './components/UserSelectionForTesting';
-import { Header } from './components/Header';
 
 export const App = () => {
-  const [pageType, setPageType] = useState('home')
-  const [questions, setQuestions] = useState([]);
-  const [userForTesting, setUserForTesting] = useState('');
+  const [pageType, setPageType] = useState('home');
   let page = null;
   switch (pageType) {
     case 'home': {
       page = <Home setPageType={setPageType} />;
-      break;
-    }
-
-    case 'admin-login': {
-      page = <Admin setPageType={setPageType} />;
-      break;
-    }
-
-    case 'admin-management': {
-      page = <Management setPageType={setPageType} />;
-      break;
-    }
-
-    case 'load': {
-      page = <LoadFile setQuestions={setQuestions} setPageType={setPageType} userForTesting={userForTesting} />;
-      break;
-    }
-
-    case 'questions': {
-      page = <Questions userForTesting={userForTesting} questions={questions} setQuestions={setQuestions} setPageType={setPageType}/>;
-      break;
-    }
-
-    case 'user-selection-for-testing': {
-      page = <UserSelectionForTesting setPageType={setPageType} setUserForTesting={setUserForTesting} />;
-      break;
-    }
-
-    case 'result': {
-      page = <Result questions={questions} userForTesting={userForTesting} setPageType={setPageType} />;
       break;
     }
 
@@ -56,7 +17,6 @@ export const App = () => {
   }
   return (
     <>
-      <Header />
       <div className={classes.container}>
         {page}
       </div>
--- src/components/UsersInput/components/SuggestionsItem/SuggestionsItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import classes from './SuggestionsItem.module.scss'
-
-export const SuggestionsItem = suggestion => {
-  return (
-    <div className={classes.container}>{suggestion.fullName}</div>
-  )
-};
-
