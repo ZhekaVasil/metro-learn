854235ae24437c616266b59da71700c2fb86ea61
Eugene <yauhen.vasileuski1@ibm.com>
2021-03-30 01:28:00 +0300

    api changes

--- backend/controllers/SectionsController.js
+++ backend/controllers/SectionsController.js
@@ -1,6 +1,11 @@
 const apiResponse = require("../helpers/apiResponse");
 const fs = require('fs');
 const path = require('path');
+const WordExtractor = require("word-extractor");
+
+const extractor = new WordExtractor();
+
+const storage = {};
 
 const sectionsPath = path.normalize('./sections')
 // const sectionsPath = path.normalize(__dirname + '/../../../sections')
@@ -22,10 +27,43 @@ exports.sectionsList = [
 ]
 
 exports.sectionsSearch = [
-	function (req, res) {
+	async function (req, res) {
+	const response = [];
 		try {
 			const term = req.params.term;
-			return apiResponse.successResponseWithData(res, 'Operation success', term)
+			Object.entries(storage || {}).forEach(([section = '', children = {}]) => {
+				Object.entries(children).forEach(([child = '', text = '']) => {
+					if (text.includes(term)) {
+						response.push({section, child})
+					}
+				})
+			})
+			return apiResponse.successResponseWithData(res, 'Operation success', response)
+		} catch (err) {
+			//throw error in json response with status 500.
+			return apiResponse.ErrorResponse(res, err);
+		}
+	}
+]
+
+exports.sectionsSync = [
+	function (req, res) {
+		try {
+			const topList = fs.readdirSync(sectionsPath);
+			topList.forEach(section => {
+				const children = fs.readdirSync(sectionsPath + `/${section}`);
+				children.forEach(async child => {
+					try {
+						const doc = await extractor.extract(sectionsPath + `/${section}/${child}`);
+						storage[section] = storage[section] || {};
+						storage[section][child] = doc.getBody();
+					} catch (error) {
+						console.error(child);
+						console.error(error);
+					}
+				})
+			})
+			return apiResponse.successResponseWithData(res, 'Operation success', 'done')
 		} catch (err) {
 			//throw error in json response with status 500.
 			return apiResponse.ErrorResponse(res, err);
--- backend/routes/sections.js
+++ backend/routes/sections.js
@@ -4,6 +4,7 @@ const SectionsController = require("../controllers/SectionsController");
 const router = express.Router();
 
 router.get("/", SectionsController.sectionsList);
+router.get("/sync", SectionsController.sectionsSync);
 router.get("/search/:term", SectionsController.sectionsSearch);
 
 module.exports = router;
